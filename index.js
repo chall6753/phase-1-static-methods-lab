43474c9c7a056c95c3e4c27586c4bf73640e276d
Charles <chall70101@gmail.com>
2021-08-20 20:30:06 -0600

    commit tree cb5e2b1f10a65961425117404cc2bae8e798c8c9

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+    let firstLetter = str[0].toUpperCase()
+    let newStr = firstLetter + str.slice(1)
+    return newStr
+  }
+  static sanitize(str){
+    return str.replace(/[!#@$%^&*(){}_+~]/g, '')
+  }
+  static titleize(str){
+    let words = str.split(' ')
+    let titleStr = ''
+    let i = 0
+    words.forEach(word => {
+      console.log(i)
+     if(word.match(/(by|for|the|but|of|at|and|from|a|an)\b/) && i != 0){
+       if(i === 0){
+        titleStr += word
+       }else{
+         console.log('yeet')
+         titleStr += ' ' + word
+       }
+       
+     }else{
+      let firstLetter = word[0].toUpperCase()
+      let newStr = firstLetter + word.slice(1)
+      console.log(i)
+      if(i === 0){
+        titleStr += newStr
+       }else{
+         titleStr += ' ' + newStr
+       }
+     }
+     i += 1
+    })
+    console.log(titleStr)
+    return titleStr
+  }
 }
